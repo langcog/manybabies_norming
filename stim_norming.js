dd9f7a3f7c197eba98ef8f423209ea615b039933
Michael C. Frank <mcfrank@stanford.edu>
2016-07-26 22:12:32 -0700

    mwe

--- stim_norming.js
+++ stim_norming.js
@@ -53,16 +53,53 @@ function shuffle(a) {
 //
 // 5. "Accent" (unfiltered). Does the speaker have an accent? (to the rater) This is to get a sense about the extent to which the clips will do a good job of sounding nativelike across at least the North American labs.
 
-var question = 'How much does this sound like it was spoken to an infant or child?';
+var conditions = [{
+  name: 'noise',
+  instructions: 'The specific question we are interested in here is <b>how noisy these clips are</b>, in particular whether they have any background noise that might keep someone from understanding the speech. We will ask you to rate the level of noise from 1 - 7, with 1 being essentially no noise (many clips might fall into this category) and 7 being too noisy to understand.',
+  question: 'Do you hear any background noise that might keep you from understanding the speech? How noisy is this audio clip?',
+  low_answer: 'No noise at all',
+  high_answer: 'Lots of noise'
+}, {
+  name: 'idsness',
+  instructions: 'The specific question we are interested in here is <b>whether these clips sound like they were spoken to a baby or young child</b>. Some of them were spoken to babies and others were spoken to adult, and we would like to know whether people can tell which is which. We will ask you to rate the clips from 1 - 7, choosing 1 if the clip was definitely spoken to an adult and 7 if the clip was definitely spoken to a baby.',
+  question: 'Does this clip sound like it was being spoken to a baby or young child?',
+  low_answer: 'Definitely spoken to an adult',
+  high_answer: 'Definitely spoken to a baby'
+}, {
+  name: 'affect',
+  instructions: 'The specific question we are interested in here is <b>how happy or sad the speaker sounds in each clip</b>. We will ask you to rate the clips from 1 - 7, choosing 1 if the speaker sounds very sad and 7 if the speaker sounds very happy. Many clips may not sound particularly happy or sad. If you think a clip does not sound happy or sad, you can choose a rating near the middle.',
+  question: 'How happy does this audio clip sound?',
+  low_answer: 'Very sad',
+  high_answer: 'Very happy'
+}, {
+  name: 'naturalness',
+  instructions: 'The specific question we are interested in here is <b>how natural the clip sounds</b>. Some clips may sound like they are being spoken by someone who is comfortably interacting with a child or an adult. Others may sound artificial or awkward, like they are being spoken by a bad actor. We will ask you to rate clips from 1 - 7, choosing 1 if the speaker sounds very unnatural and 7 if they sound very natural or normal.',
+  question: 'Does this clip sound like the speaker is comfortable and in a normal environment, or like the speaker is awkward or artificial? How natural-sounding is this clip?',
+  low_answer: 'Very unnatural',
+  high_answer: 'Very natural'
+}, {
+  name: 'accent',
+  instructions: 'The specific question we are interested in here is <b>whether the speakers in these clips sound to you like they have a particular accent or whether they sound normal and unaccented</b>. We will ask you to rate clips from 1 - 7, choosing 1 if the speaker sounds like they have no accent or sound perfectly normal and 7 if the speaker sounds like she has a very strong accent. Many clips may sound unaccented. If they sound like they have no accent, it is OK to rate many of them near 1.',
+  question: 'Does this speaker sound like she has an accent?',
+  low_answer: 'No accent',
+  high_answer: 'Very strong accent'
+}]
 
-var trials = [];
-for (i = 1; i <= 20; i++) {
-  trials.push(i + ".wav")
+var condition = conditions.random();
+
+var filenames = [];
+for (i = 1; i <= 2; i++) {
+  filenames.push(i + ".wav")
 }
 
-trials = shuffle(trials);
-var numTrialsExperiment = trials.length;
+filenames.push("choose_1.wav")
+filenames.push("choose_4.wav")
+filenames.push("choose_7.wav")
 
+filenames = shuffle(filenames);
+var numTrialsExperiment = filenames.length;
+
+var audio_played = false;
 
 // Show the instructions slide -- this is what we want subjects to see first.
 showSlide("instructions");
@@ -72,14 +109,14 @@ var experiment = {
 
   // The object to be submitted.
   data: {
-    trial_number_block: [],
-    trial_type: [],
-    sentence: [],
+    condition: condition.name,
+    filename: [],
     rating: [],
     ladder: [],
     age: [],
     gender: [],
     education: [],
+		region: [],
     homelang: [],
     ethnicity: [],
     race: [],
@@ -103,33 +140,50 @@ var experiment = {
 
     var response_logged = false;
 
-
     //Array of radio buttons
     var radio = document.getElementsByName("judgment");
 
     // Loop through radio buttons
     for (i = 0; i < radio.length; i++) {
       if (radio[i].checked) {
-        experiment.data.rating.push(radio[i].value);
+				var response_value = radio[i].value;
         response_logged = true;
       }
     }
 
-    if (response_logged) {
+    // now deal with going next if audio has played and there is a response.
+    if (response_logged & audio_played) {
       nextButton.blur();
 
       // uncheck radio buttons
       for (i = 0; i < radio.length; i++) {
-        radio[i].checked = false
+        radio[i].checked = false;
       }
+
+			audio_played = false;
+			experiment.data.rating.push(response_value);
       experiment.next();
-    } else {
+
+    } else if (audio_played) {
       $("#testMessage").html('<font color="red">' +
         'Please make a response!' +
         '</font>');
-    }
+    } else if (response_logged) {
+			$("#testMessage").html('<font color="red">' +
+				'Please listen to the whole clip before responding!' +
+				'</font>');
+		} else {
+			$("#testMessage").html('<font color="red">' +
+				'Please listen to the whole clip and then make a response!' +
+				'</font>');
+		}
   },
 
+  // specific instructions
+  instructions: function() {
+    showSlide("specific_instructions"); //display slide
+    $("#instructions_text").html(condition.instructions); //add sentence to html
+  },
 
   // The work horse of the sequence - what to do on every trial.
   next: function() {
@@ -138,36 +192,35 @@ var experiment = {
     if (window.self == window.top | turk.workerId.length > 0) {
 
       $("#progress").attr("style", "width:" +
-        String(100 * (1 - (trials.length) / numTrialsExperiment)) + "%")
+        String(100 * (1 - (filenames.length) / numTrialsExperiment)) + "%")
 
       //style="width:progressTotal%"
 
       // Get the current trial - <code>shift()</code> removes the first element
       // select from our scales array and stop exp after we've exhausted all the domains
-      var this_trial = trials.shift();
+      var this_file = filenames.shift();
 
       //If the current trial is undefined, call the end function.
-      if (typeof this_trial == "undefined") {
+      if (typeof this_file == "undefined") {
         return experiment.debriefing();
       }
 
       showSlide("norm_slide"); //display slide
-      $("#question").html(question); //add sentence to html
+      $("#question").html(condition.question); //add sentence to html
+      $("#low_answer").html(condition.low_answer); //add sentence to html
+      $("#high_answer").html(condition.high_answer); //add sentence to html
 
       var audio = document.getElementById('audio');
       // var sourceOgg=document.getElementById('player');
       var sourceMp3 = document.getElementById('audio');
 
-      sourceMp3.src = 'wavs/' + this_trial;
+      sourceMp3.src = 'wavs/' + this_file;
 
       audio.load(); //just start buffering (preload)
       // audio.play(); //start playingvar audio = document.getElementById('audio');
 
-
-      // log the sentence for each trial
-      experiment.data.sentence.push(trial_info.sentence);
-      experiment.data.trial_type.push(trial_info.trial_type)
-      experiment.data.trial_number_block.push(trial_info.trial_number_block)
+      // log the file for each trial
+      experiment.data.filename.push(this_file);
     }
   },
 
@@ -189,11 +242,11 @@ var experiment = {
         experiment.data.race.push(races[i].value);
       }
     }
-    experiment.data.ladder.push(document.getElementById("ladder").value);
+    // experiment.data.ladder.push(document.getElementById("ladder").value);
     experiment.data.age.push(document.getElementById("age").value);
     experiment.data.gender.push(document.getElementById("gender").value);
     experiment.data.education.push(document.getElementById("education").value);
-    experiment.data.homelang.push(document.getElementById("homelang").value);
+    experiment.data.region.push(document.getElementById("region").value);    experiment.data.homelang.push(document.getElementById("homelang").value);
     experiment.data.ethnicity.push(document.getElementById("ethnicity").value);
     experiment.data.children.push(document.getElementById("children").value);
     experiment.data.childAgeYoung.push(document.getElementById("youngestAge").value);
